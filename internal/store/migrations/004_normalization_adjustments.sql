98cb55f298d5af9b2b587b1962c6bbfde0b8716f
Addison Emig <addison@kwila.dev>
2026-07-18 17:29:31 -0500

    fix: combine normalization migrations

--- internal/store/migrations/004_normalization_adjustments.sql
+++ internal/store/migrations/004_normalization_adjustments.sql
@@ -3,6 +3,3 @@ ADD COLUMN normalization_gate_adjustment INTEGER NOT NULL DEFAULT 0;
 
 ALTER TABLE sermons
 ADD COLUMN normalization_volume_adjustment INTEGER NOT NULL DEFAULT 0;
-
-ALTER TABLE sermons
-DROP COLUMN normalization_preset;
--- internal/store/migrations/004_normalization_preset.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE sermons
-ADD COLUMN normalization_preset TEXT NOT NULL DEFAULT 'standard';
